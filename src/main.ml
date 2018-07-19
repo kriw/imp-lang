514307fe7e4f444fc237f242711f9d19175d7565
kriw <kotarou777775@gmail.com>
2018-07-19 09:42:34 +0900

    Add declare vars

--- src/main.ml
+++ src/main.ml
@@ -1,15 +1,28 @@
-let main () =
+let read_statement () =
     let stdinbuf = Lexing.from_channel stdin in
     try
         let stmt = (Parser.parse Lexer.token stdinbuf) in
+        Some stmt
+    with
+  | Lexer.Error msg ->
+          let _ = Printf.eprintf "%s%!" msg in
+          None
+  | Parser.Error ->
+          let _ = Printf.eprintf "At offset %d: syntax error.\n%!" (Lexing.lexeme_start stdinbuf) in
+          None
+
+let interpreter () =
+    match read_statement () with
+    | Some stmt ->
         let _ = Printf.printf "%s\n" (Syntax.show_statement stmt) in
         let _ = Interpreter.eval_statement stmt in
         let _ = Printf.printf "Env:\n" in
         Interpreter.Env.iter (fun k v -> Printf.printf "%s = %s\n" k (Syntax.show_const v)) !Interpreter.env
-    with
-  | Lexer.Error msg ->
-          Printf.eprintf "%s%!" msg
-  | Parser.Error ->
-          Printf.eprintf "At offset %d: syntax error.\n%!" (Lexing.lexeme_start stdinbuf)
+    | None -> Printf.eprintf "Failed to parse"
 
-let () = main ()
+let ir () = 
+    match read_statement () with
+    | Some stmt -> Ir.compile stmt
+    | None -> ()
+
+let () = ir ()
--- src/middle/ir.ml
+++ src/middle/ir.ml
@@ -17,25 +17,95 @@ type opcode =
   | Or
   | Lt
   | LtEq
+[@@deriving show]
+
+let node_id = ref 0
+let edge_id = ref 0
+
+let new_node_id =
+    let ret = !node_id in
+    let _ = node_id := ret + 1 in
+    ret
+
+let new_edge_id =
+    let ret = !edge_id in
+    let _ = edge_id := ret + 1 in
+    ret
+
+type nodeId = int
+[@@deriving show]
+
+type edgeId = int
+[@@deriving show]
+
+type valueNode =
+    | ConstInt of int
+    | ConstBool of bool
+    | Variable of string
+[@@deriving show]
 
 type node =
     | EntryNode
-    | ActionNode of opcode
-    | ValueNode of string
+    | ActionNode of nodeId * opcode
+    | ValueNode of nodeId * valueNode
+[@@deriving show]
+
 type edge =
-    | NextEdge of node * node
-    | ValueEdge of node * node
-    | JmpEdge of node * node
+    | NextEdge of edgeId * node * node
+    | ValueEdge of edgeId * node * node
+    | JmpEdge of edgeId * node * node
+[@@deriving show]
 
 let edges = ref []
 let nodes = ref [EntryNode]
 
 let add_edge e = edges := e :: !edges
 let add_node n = nodes := n :: !nodes
+let find_edge eid =
+    let rec find_rec edges =
+        match edges with
+        | NextEdge (id, _, _) :: _ when id == eid -> Some (List.hd edges)
+        | JmpEdge (id, _, _) :: _ when id == eid -> Some (List.hd edges)
+        | ValueEdge (id, _, _) :: _ when id == eid -> Some (List.hd edges)
+        | _ :: _ -> find_rec edges
+        | _ -> None in
+    find_rec !edges
+
+let find_node nid =
+    let rec find_rec nodes =
+        match nodes with
+        | ActionNode (id, _) :: _ when id == nid -> Some (List.hd nodes)
+        | ValueNode (id, _) :: _ when id == nid -> Some (List.hd nodes)
+        | _ :: ns -> find_rec ns
+        | _ -> None in
+    find_rec !nodes
+
+let new_var name =
+    let nid = new_node_id in
+    let node = ValueNode (nid, Variable name) in
+    let _ = add_node node in
+    nid
 
 module Vars = Map.Make(String);;
 
-let vars = ref Vars.empty
+let init_vars vars =
+    let declare var_name = let _ = new_var var_name in () in
+    List.iter declare vars
+
+let rec gather_vars s =
+    match s with
+    | Syntax.Define (i, _) -> [i]
+    | Syntax.If (_, if_then, Some if_else) ->
+            let vars1 = gather_vars if_then in
+            let vars2 = gather_vars if_else in
+            List.append vars1 vars2
+    | Syntax.If (_, if_then, None) -> gather_vars if_then
+    | Syntax.While (_, st) -> gather_vars st
+    | Syntax.Seq (s1, s2) ->
+        let vars1 = gather_vars s1 in
+        let vars2 = gather_vars s2 in
+        List.append vars1 vars2
+    | _ -> []
 
 let emit_operator op x y =
     match op with
@@ -69,3 +139,10 @@ let rec emit_statement s =
         let _ = emit_statement s2 in
         raise TODO
     | Syntax.Emp -> ""
+
+let compile s =
+    let vs = gather_vars s in
+    let _ = init_vars vs in
+    let _ = List.iter (fun n -> Printf.printf "%s\n" (show_node n)) !nodes in
+    ()
+
